dc2fadfdb40a72ac1dfb1e822c1aa186c6149c17
Srdjan S <stankovic.srdjo@gmail.com>
2020-03-12 20:25:06 +0100

    Add user login and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <Switch>
+        <Route path="/register">
+          <SignUp></SignUp>
+        </Route>
+        <Route path="/login">
+          <SignIn></SignIn>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
